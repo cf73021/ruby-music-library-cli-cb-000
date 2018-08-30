12285e4264e436b53da41437c03e4fed83af3554
Chris Franklin <cf73021@gmail.com>
2018-08-30 02:30:44 +0000

    automatically backed up by learn

--- lib/concerns/concerns::findable.rb
+++ lib/concerns/concerns::findable.rb
@@ -3,6 +3,16 @@ module Concerns::Findable
     self.all.find{|obj| obj.name == name}
   end
 
+  def create_by_name(name)
+    create(name)
+  end
+
+  def self.create(name)
+    new_song = Song.new(name)
+    new_song.save
+    new_song
+  end
+
   def find_or_create_by_name(name)
     found = find_by_name(name)
     if found != nil
