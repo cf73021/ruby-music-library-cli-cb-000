4fdc6b43c878980d2b3dda13aa8ffe5fcce2f5a5
Chris Franklin <cf73021@gmail.com>
2018-08-30 02:07:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    create_by_name(name)  if !find_by_name(name) 
+    create_by_name(name)  if !find_by_name(name)
   end
 
   def self.find_by_name(name)
