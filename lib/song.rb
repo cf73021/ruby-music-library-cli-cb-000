31b018bda7fa0a410f047787e6407700540a5d3a
Chris Franklin <cf73021@gmail.com>
2018-08-30 02:42:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,4 +55,12 @@ class Song
     self.all.find{|song| song.name == name}
   end
 
+  def self.new_from_filename(filename)
+  artist_s, name_s, genre_s = filename.gsub(".mp3", "").split(" - ")
+  song = find_or_create_by_name(name_s.strip)
+  song.artist = Artist.find_or_create_by_name(artist_s.strip)
+  song.genre = Genre.find_or_create_by_name(genre_s.strip)
+  song
+end
+
 end
