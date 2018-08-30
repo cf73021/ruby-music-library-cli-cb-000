dd787816e3da2f6792fc0e23973b4528017a4ecc
Chris Franklin <cf73021@gmail.com>
2018-08-30 02:30:21 +0000

    automatically backed up by learn

--- lib/concerns/concerns::findable.rb
+++ lib/concerns/concerns::findable.rb
@@ -2,4 +2,13 @@ module Concerns::Findable
   def find_by_name(name)
     self.all.find{|obj| obj.name == name}
   end
+
+  def find_or_create_by_name(name)
+    found = find_by_name(name)
+    if found != nil
+    then
+      return found
+    else
+      create_by_name(name)
+    end
 end
