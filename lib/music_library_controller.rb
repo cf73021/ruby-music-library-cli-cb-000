f173a1e76565e7c24156964a952462fcd31e09e3
Chris Franklin <cf73021@gmail.com>
2018-08-30 03:10:12 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,6 +20,20 @@ class MusicLibraryController
       puts "What would you like to do?"
       answer = gets.chomp
     end
+    case answer
+        when "list songs"
+          list_songs
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
+        when "play song"
+          play_song
+        end
   end
 
   def list_songs
